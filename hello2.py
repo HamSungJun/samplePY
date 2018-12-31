c0cfbe229bc23177f5e4579596dd16579437a2be
HamSungJun <wleo12345@naver.com>
2018-12-31 11:18:48 +0900

    mod src

--- hello2.py
+++ hello2.py
@@ -7,8 +7,17 @@ class hello_machine2:
     def hello_23(self):
     	return "Hello2"
 
-a = hello_machine()
+a = hello_machine2()
 
+class hi_maam:
+  def __init__(self):
+    return
+  
+  def holllo(self):
+    a = 3 + 2
+    return a
+   
+    
 print(a.hello_23())
 print(a.hello_23())
 
