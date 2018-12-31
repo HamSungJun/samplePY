d53027c6231332fd1cf578b589890e2384b01d21
HamSungJun <wleo12345@naver.com>
2018-12-31 10:58:48 +0900

    build shell

---
+++ hello2.py
@@ -0,0 +1,15 @@
+class hello_machine2:
+    def __init__(self):
+        return
+
+    def hello_12(self):
+        print("Hello")
+    def hello_23(self):
+    	return "Hello2"
+
+a = hello_machine()
+
+print(a.hello_23())
+print(a.hello_23())
+
+print("newwwwwwwwwwwwwwwwwwwwww")
